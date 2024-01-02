f42a1f1cb2087d64771ff591c5f4784e5e7fe0e9
Maria Monpeat <115364869+mmonpeat@users.noreply.github.com>
2024-01-02 19:53:10 +0100

    :)

--- ExamRank04/microshell.c
+++ ExamRank04/microshell.c
@@ -29,9 +29,9 @@ int main (int ac, char **av, char **env)
 int cd(char **av, int i)
 {
 	if ( i != 2)
-		return (error("error: cd: bad arguments\n");
+		return (err("err: cd: bad arguments\n"));
 	else if (chdir(av[1]) == -1)
-		return (error("error: cd: connot change directory to ", error(av[1]), error(av[1]);
+		return (err("eor: cd: connot change directory to ", err(av[1]), err(av[1])));
 	return (0);
 }
 
@@ -50,20 +50,20 @@ int executor(char **av, int i)
 	int has_pipe = is_pipe(av, i);
 	*/
 	if (has_pipe && pipe(fd) == -1)//si hi ha una pipe, crea la pipe amb pipe()
-		return (error("error: fatal\n"));
+		return (err("error: fatal\n"));
 	
 	int pid = fork();
 	if (pid == 0)
 	{
 		av[i] = 0;//Afegim un 0 al final del array d'arguments per indicar el final de la comanda
 		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1] == -1))
-				return (error("error: fatal\n"));
+				return (err("error: fatal\n"));
 		execve(*av, av, env);
-		return (error("error: cannot execute ", error(*av), error("\n"));
+		return (error("error: cannot execute ", err(*av), err("\n"));
 	}
 	waitpid(pid, &status, 0);
 	if (has_pipe && (dup(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-		return (error("error: fatal\n");
+		return (err("error: fatal\n");
 	return (WIFEXITED(status) && WEXITSTATUS(status));
 	//La macro WIFEXITED comprova si el procés fill ha finalitzat normalment, i WEXITSTATUS proporciona el codi de sortida del procés fill.
 }
