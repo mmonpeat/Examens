634eb272f1bdb7bb10f95c1a2b8e84ed00bee04e
Maria Monpeat <115364869+mmonpeat@users.noreply.github.com>
2024-01-02 19:40:37 +0100

    :)

--- ExamRank04/microshell.h
+++ ExamRank04/microshell.h
@@ -1,5 +1,13 @@
 #ifndef MICROSHELL_H
 # define MICROSHELL_H
 
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int ac, char **av, char **env);
+int cd(char **av, int i);
+int executor(char **av, char **env, int i);
+int error(char *str);
 
 #endif
