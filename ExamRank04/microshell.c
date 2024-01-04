1159a831ac905160139621c0c47d612112b828db
Maria Monpeat <115364869+mmonpeat@users.noreply.github.com>
2024-01-04 07:52:46 +0100

    Update microshell.c

--- ExamRank04/microshell.c
+++ ExamRank04/microshell.c
@@ -1,91 +1,12 @@
 #include "microshell.h"
 
-int fd_putstr(int fd, char *str)
-{
-  int i;
-
-  i = 0;
-  while (str[i])
-    write(fd, &str[i++], 1);
-  return (i);
-}
-
-void  is_cd(t_micro *sh, char **av)
-{
-  if (sh->i != 2)
-    fd_putstr(2, "error: cd: bad arguments\n");
-  else if (chdir(av[1]) == ERROR)
-  {
-    fd_putstr(2, "error: cd: cannot change directory to ");
-    fd_putstr(2, av[1]);
-    fd_putstr(2, "\n");
-  }
-}
-
-int ft_execute(t_micro *sh, char **av, char **env)
-{
-  av[sh->i] = NULL;
-  dup2(sh->tmp_fd, 0);
-  close(sh->tmp_fd);
-  execve(av[0], av, env);
-  fd_putstr(2, "error: cannot execute ");
-  fd_putstr(2, av[0]);
-  fd_putstr(2, "\n");
-  return (E_EXIT);
-}
-
-int no_pipe(t_micro *sh, char **av, char **env)
-{
-  sh->n_p++;
-  sh->pid = fork();
-  if (sh->pid == ERROR)
-      return (E_EXIT);
-  if (!sh->pid)
-  {
-    if (ft_execute(sh, av, env))
-      return (E_EXIT);
-  }
-  else
-  {
-    close(sh->tmp_fd);
-    while (sh->n_p-- > 0)
-      waitpid(-1, NULL, WUNTRACED);
-    sh->tmp_fd = dup(0);
-    sh->n_p = 0;
-  }
-  return (EXIT_SUCCESS);
-}
-int is_pipe(t_micro *sh, char **av, char **env)
-{
-  sh->n_p++;
-  pipe(sh->fd);
-  sh->pid = fork();
-  if (sh->pid == ERROR)
-    return (E_EXIT);
-  if (!sh->pid)
-  {
-    dup2(sh->fd[1], 1);
-    close(sh->fd[0]);
-    close(sh->fd[1]);
-    if (ft_execute(sh, av, env))
-      return (E_EXIT);
-  }
-  else
-  {
-    close(sh->fd[1]);
-    close(sh->tmp_fd);
-    sh->tmp_fd = sh->fd[0];
-  }
-  return (EXIT_SUCCESS);
-}
-
 int main(int ac, char **av, char **env)
 {
   t_micro sh;
 
   (void)ac;
   sh.i = 0;
-  sh.n_p = 0;
+  sh.num_pross = 0;
   sh.tmp_fd = dup(0);
   while (av[sh.i] && av[sh.i + 1])
   {
@@ -108,4 +29,85 @@ int main(int ac, char **av, char **env)
   }
  close(sh.tmp_fd);
   return (EXIT_SUCCESS);
+}
+
+int is_pipe(t_micro *sh, char **av, char **env)
+{
+  sh->num_pross++;
+  pipe(sh->fd);
+  sh->pid = fork();
+  if (sh->pid == ERROR)
+    return (E_EXIT);
+  if (!sh->pid)
+  {
+    dup2(sh->fd[1], 1);
+    close(sh->fd[0]);
+    close(sh->fd[1]);
+    if (ft_execute(sh, av, env))
+      return (E_EXIT);
+  }
+  else
+  {
+    close(sh->fd[1]);
+    close(sh->tmp_fd);
+    sh->tmp_fd = sh->fd[0];
+  }
+  return (EXIT_SUCCESS);
+}
+
+
+int no_pipe(t_micro *sh, char **av, char **env)
+{
+  sh->num_pross++;
+  sh->pid = fork();
+  if (sh->pid == ERROR)
+      return (E_EXIT);
+  if (!sh->pid)
+  {
+    if (ft_execute(sh, av, env))
+      return (E_EXIT);
+  }
+  else
+  {
+    close(sh->tmp_fd);
+    while (sh->n_p-- > 0)
+      waitpid(-1, NULL, WUNTRACED);
+    sh->tmp_fd = dup(0);
+    sh->num_pross = 0;
+  }
+  return (EXIT_SUCCESS);
+}
+
+int ft_execute(t_micro *sh, char **av, char **env)
+{
+  av[sh->i] = NULL;
+  dup2(sh->tmp_fd, 0);
+  close(sh->tmp_fd);
+  execve(av[0], av, env);
+  fd_putstr(2, "error: cannot execute ");
+  fd_putstr(2, av[0]);
+  fd_putstr(2, "\n");
+  return (E_EXIT);
+}
+
+void  is_cd(t_micro *sh, char **av)
+{
+  if (sh->i != 2)
+    fd_putstr(2, "error: cd: bad arguments\n");
+  else if (chdir(av[1]) == ERROR)
+  {
+    fd_putstr(2, "error: cd: cannot change directory to ");
+    fd_putstr(2, av[1]);
+    fd_putstr(2, "\n");
+  }
+}
+
+int fd_putstr(int fd, char *str)
+{
+  int i;
+
+  i = 0;
+  while (str[i])
+    write(fd, &str[i++], 1);
+  return (i);
 }
