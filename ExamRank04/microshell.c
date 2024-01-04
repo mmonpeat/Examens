13fd867aeb90e7f4fe31c8d1ba97d028853a67f8
Maria Monpeat <115364869+mmonpeat@users.noreply.github.com>
2024-01-04 08:53:49 +0100

    Update microshell.c

--- ExamRank04/microshell.c
+++ ExamRank04/microshell.c
@@ -31,6 +31,40 @@ int main(int ac, char **av, char **env)
   return (EXIT_SUCCESS);
 }
 
+void  is_cd(t_micro *sh, char **av)
+{
+  if (sh->i != 2)
+    fd_putstr(2, "error: cd: bad arguments\n");
+  else if (chdir(av[1]) == ERROR)
+  {
+    fd_putstr(2, "error: cd: cannot change directory to ");
+    fd_putstr(2, av[1]);
+    fd_putstr(2, "\n");
+  }
+}
+
+int no_pipe(t_micro *sh, char **av, char **env)
+{
+  sh->num_pross++;
+  sh->pid = fork();
+  if (sh->pid == ERROR)
+      return (E_EXIT);
+  if (!sh->pid)
+  {
+    if (ft_execute(sh, av, env))
+      return (E_EXIT);
+  }
+  else
+  {
+    close(sh->tmp_fd);
+    while (sh->n_p-- > 0)
+      waitpid(-1, NULL, WUNTRACED);
+    sh->tmp_fd = dup(0);
+    sh->num_pross = 0;
+  }
+  return (EXIT_SUCCESS);
+}
+
 int is_pipe(t_micro *sh, char **av, char **env)
 {
   sh->num_pross++;
@@ -55,29 +89,6 @@ int is_pipe(t_micro *sh, char **av, char **env)
   return (EXIT_SUCCESS);
 }
 
-
-int no_pipe(t_micro *sh, char **av, char **env)
-{
-  sh->num_pross++;
-  sh->pid = fork();
-  if (sh->pid == ERROR)
-      return (E_EXIT);
-  if (!sh->pid)
-  {
-    if (ft_execute(sh, av, env))
-      return (E_EXIT);
-  }
-  else
-  {
-    close(sh->tmp_fd);
-    while (sh->n_p-- > 0)
-      waitpid(-1, NULL, WUNTRACED);
-    sh->tmp_fd = dup(0);
-    sh->num_pross = 0;
-  }
-  return (EXIT_SUCCESS);
-}
-
 int ft_execute(t_micro *sh, char **av, char **env)
 {
   av[sh->i] = NULL;
@@ -88,18 +99,6 @@ int ft_execute(t_micro *sh, char **av, char **env)
   fd_putstr(2, av[0]);
   fd_putstr(2, "\n");
   return (E_EXIT);
-}
-
-void  is_cd(t_micro *sh, char **av)
-{
-  if (sh->i != 2)
-    fd_putstr(2, "error: cd: bad arguments\n");
-  else if (chdir(av[1]) == ERROR)
-  {
-    fd_putstr(2, "error: cd: cannot change directory to ");
-    fd_putstr(2, av[1]);
-    fd_putstr(2, "\n");
-  }
 }
 
 int fd_putstr(int fd, char *str)
