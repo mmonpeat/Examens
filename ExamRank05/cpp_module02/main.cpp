d4b153b481f5c263422577d8be55688e3ad3f874
Maria Monpeat <115364869+mmonpeat@users.noreply.github.com>
2025-03-24 19:20:58 +0100

    finished?

--- ExamRank05/cpp_module02/main.cpp
+++ ExamRank05/cpp_module02/main.cpp
@@ -1,21 +1,33 @@
-#include "Warlock.hpp"
 #include "ASpell.hpp"
-#include "ATarget.hpp"
 #include "Dummy.hpp"
+#include "Polymorph.hpp"
+#include "Warlock.hpp"
+#include "ATarget.hpp"
+#include "Fireball.hpp"
+#include "BrickWall.hpp"
 #include "Fwoosh.hpp"
+#include "TargetGenerator.hpp"
 
 int main()
 {
-  Warlock richard("Richard", "the Titled");
+  Warlock richard("Richard", "foo");
+  richard.setTitle("Hello, I'm Richard the Warlock!");
+  BrickWall model1;
 
-  Dummy bob;
-  Fwoosh* fwoosh = new Fwoosh();
+  Polymorph* polymorph = new Polymorph();
+  TargetGenerator tarGen;
 
-  richard.learnSpell(fwoosh);
+  tarGen.learnTargetType(&model1);
+  richard.learnSpell(polymorph);
+
+  Fireball* fireball = new Fireball();
+
+  richard.learnSpell(fireball);
+
+  ATarget* wall = tarGen.createTarget("Inconspicuous Red-brick Wall");
 
   richard.introduce();
-  richard.launchSpell("Fwoosh", bob);
-
-  richard.forgetSpell("Fwoosh");
-  richard.launchSpell("Fwoosh", bob);
+  richard.launchSpell("Polymorph", *wall);
+  richard.launchSpell("Fireball", *wall);
 }
+
