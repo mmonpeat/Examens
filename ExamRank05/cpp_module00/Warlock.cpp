e32f724dc9707a7181002055ec63ac87fc953e45
Maria Monpeat <115364869+mmonpeat@users.noreply.github.com>
2025-03-24 18:13:16 +0100

    finished

---
+++ ExamRank05/cpp_module00/Warlock.cpp
@@ -0,0 +1,45 @@
+#include "Warlock.hpp"
+
+Warlock::Warlock(void) {}
+
+Warlock::Warlock(const Warlock &obj)
+{
+	*this = obj;
+}
+
+Warlock &Warlock::operator=(const Warlock &obj)
+{
+	this->name = obj.name;
+	this->title = obj.title;
+	return (*this);
+}
+
+Warlock::Warlock(std::string name, std::string title): name(name), title(title)
+{
+	std::cout << name << ": This looks like another boring day." << std::endl;
+}
+
+Warlock::~Warlock()
+{
+	std::cout << name << ": My job here is done!" << std::endl;
+}
+
+std::string const &Warlock::getName() const
+{
+	return (name);
+}
+
+std::string const &Warlock::getTitle() const
+{
+	return (title);
+}
+
+void Warlock::setTitle(const std::string &title)
+{
+	this->title = title;
+}
+
+void Warlock::introduce() const
+{
+	std::cout << name << ": I am " << name << ", " << title << "!" << std::endl;
+}
---
+++ ExamRank05/cpp_module00/Warlock.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+
+class Warlock
+{
+	private:
+		std::string	name;
+		std::string	title;
+
+		Warlock();
+		Warlock(const Warlock &obj);
+		Warlock &operator=(Warlock const &obj);
+	
+	public:
+		Warlock(std::string const name, std::string const title);
+		~Warlock();
+
+		std::string const &getName() const;
+		std::string const &getTitle() const;
+
+		void setTitle(std::string const &title);
+		void introduce() const;
+};
